9344e453055b0b013382659a7dc713496368ae25
bimapriaaditya <bpabima@gmail.com>
2022-09-01 17:07:20 +0700

    change folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles()
 {
-  return src('index.scss')
+  return src('beam/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask()
 {
-  watch(['index.scss'], buildStyles);
+  watch(['beam/**/*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
